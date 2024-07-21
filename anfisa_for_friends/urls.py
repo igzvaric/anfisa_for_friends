93ca2e722cbcba11c2e3d89915770d46bf3a7cd5
Igor Zvarichuk <me@zvarichuk.ru>
2024-07-21 13:40:06 +0300

    One more sprint

---
+++ anfisa_for_friends/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('homepage.urls')),
+    path('about/', include('about.urls')),
+    path('ice_cream/', include('ice_cream.urls')),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
